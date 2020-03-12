20dab3b23e1e1598bf2bf66c28f8b35707d1b839
Caleb Trachte <cetrachte@hotmail.com>
2020-03-12 15:45:37 -0500

    changed worker to use fetch instead of homegrown solution, some QOL improvements too

--- application/worker.js
+++ application/worker.js
@@ -1,23 +1,14 @@
 
 function worker_function() {
-  var HttpClient = function() {
-      this.get = function(aUrl, aCallback) {
-          var anHttpRequest = new XMLHttpRequest();
-          anHttpRequest.onreadystatechange = function() {
-              if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                  aCallback(anHttpRequest.responseText);
-          }
-
-          anHttpRequest.open( "GET", aUrl, true );
-          anHttpRequest.send( null );
-      }
-  }
-
-  var client = new HttpClient();
-    client.get('https://api.darksky.net/forecast/369e1a0d03a6d8ad3076a8c258c6c28c/34.840926825303605, -92.25945627583154', function(response) {
-      console.log(response);
-      postMessage(response);
-    });
+  let data = fetch('https://api.darksky.net/forecast/369e1a0d03a6d8ad3076a8c258c6c28c/34.840926825303605, -92.25945627583154').then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+    postMessage(data.currently.summary);
+  }).catch(function() {
+    console.log("ERROR FETCHING WEATHER DATA");
+    postMessage("ERROR FETCHING WEATHER DATA");
+  });
 }
 // This is in case of normal worker start
 // "window" is not defined in web worker
